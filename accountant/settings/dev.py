1fa25417e1989273a8fb438762b769c23a545d22
Pierre Dulac <dulacpier@gmail.com>
2015-02-01 20:10:07 +0100

    Add dev settings for the loggings

--- accountant/settings/dev.py
+++ accountant/settings/dev.py
@@ -31,3 +31,40 @@ INSTALLED_APPS += (
 # Emailing
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+
+# Logging
+
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "handlers": {
+        "console": {
+            "level": "INFO",
+            "class": "logging.StreamHandler",
+        },
+    },
+    "loggers": {
+        "django": {
+            "handlers": ["console"],
+            "propagate": True,
+            "level": "WARNING",
+        },
+        "django.request": {
+            "handlers": ["console"],
+            "level": "WARNING",
+            "propagate": False,
+        },
+        "django.db.backends": {
+            "handlers": ["console"],
+            "propagate": False,
+            "level": "WARNING",
+        },
+
+        "apps": {
+            "handlers": ["console"],
+            "level": "WARNING",
+            "propagate": False,
+        },
+    }
+}
